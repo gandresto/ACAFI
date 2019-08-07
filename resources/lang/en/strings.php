344af25a5ed0240be5356c742e3c5ef8bd92fae0
Andrés González <gandresto@outlook.com>
2019-08-06 23:01:18 -0500

    Nuevas traducciones.

--- resources/lang/en/strings.php
+++ resources/lang/en/strings.php
@@ -1,8 +1,12 @@
 <?php
 
 return [
-    'login' => 'Iniciar sesión',
-    'logout' => 'Cerrar sesión',
-    'register' => 'Registrarse',
-    'send_pass_reset_link' => 'Enviar link de cambio de contraseña',
+    'login' => 'Login',
+    'logout' => 'Logout',
+    'register' => 'Register',
+    'remember_me' => 'Remember me',
+    'email' => 'E-Mail',
+    'password' => 'Password',
+    'forg_password' => 'Forgot Your Password?',
+    'send_pass_reset_link' => 'Send password reset link',
 ];
--- resources/lang/es/strings.php
+++ resources/lang/es/strings.php
@@ -4,5 +4,9 @@ return [
     'login' => 'Iniciar sesión',
     'logout' => 'Cerrar sesión',
     'register' => 'Registrarse',
+    'remember_me' => 'Recuérdame',
+    'email' => 'Correo',
+    'password' => 'Contrseña',
+    'forg_password' => '¿Olvidaste tu contraseña?',
     'send_pass_reset_link' => 'Enviar link de cambio de contraseña',
 ];
