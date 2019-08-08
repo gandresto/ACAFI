bf09292cf0508911a57f1334e1d079941095fc18
Andrés González <gandresto@outlook.com>
2019-08-08 15:40:20 -0500

    Corregir rm fallido

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email', 50)->unique();
+            $table->string('password');
+            $table->unsignedBigInteger('academico_id')->unique();
+            $table->rememberToken();
+            $table->timestamps();
+
+            $table->index('academico_id');
+        });
+
+        #DB::update("ALTER TABLE users AUTO_INCREMENT = 2;");
+        App\User::create([
+            'id' => 1,
+            'email' => config('admin.email'),
+            'password' => bcrypt(config('admin.password')),
+            'academico_id' => 1,
+        ]);
+
+        /* DB::table('users')->insert([
+            [
+                'id' => 1,
+                'email'=> config('admin.login'),
+                'password'=> Hash::make(config('admin.password')),
+                'academico_id' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:m:s'),
+            ],
+        ]); */
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
