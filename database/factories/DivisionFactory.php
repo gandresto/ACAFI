bb21416aeb10a967758f8a9152d1fead71fa0bb2
Andrés González <gandresto@outlook.com>
2019-08-14 01:08:34 -0500

    Añadí seeders para división y departamento.

---
+++ database/factories/DivisionFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+use App\Division;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(Division::class, function (Faker $faker) {
+    return [
+        'siglas' => 'D'.strtoupper(Str::random(3)),
+        'nombre' => $faker->sentence(6, true),
+        'url' => $faker->url,
+        'logo' => '/storage/' . Str::random(10) . '.png',
+        'jefe_div_id' => $faker->unique()->numberBetween($min = 2, $max = 50),
+    ];
+});
