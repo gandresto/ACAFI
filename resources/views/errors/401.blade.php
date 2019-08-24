a778e2e34bffd924043f50f2489030f632cc5cad
Andrés González <gandresto@outlook.com>
2019-08-23 21:47:16 -0500

    Publicar vistas de errores http.

---
+++ resources/views/errors/401.blade.php
@@ -0,0 +1,5 @@
+@extends('errors::minimal')
+
+@section('title', 'No autorizado')
+@section('code', '401')
+@section('message', 'No estás autorizado para ver esta página.')
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,5 @@
+@extends('errors::minimal')
+
+@section('title', 'No encontrado')
+@section('code', '404')
+@section('message', 'El recurso al que intenas acceder no está disponible, verifica la ruta.')
