c7df2163526e8a3d8282d8f925347d54c2e6574f
Andrés González <gandresto@outlook.com>
2019-09-14 06:27:17 -0500

    Añadí soporte a rutas https.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(240);
+        if(env('FORCE_HTTPS')) {
+            URL::forceScheme('https');
+        }
     }
 }
