00ca999d4ce32c9698bd9f1482eaa35d217d9ba1
Andrés González <gandresto@outlook.com>
2019-09-19 17:31:20 -0500

    Default string length a 191

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(240);
+        Schema::defaultStringLength(191);
         //if(env('FORCE_HTTPS')) {
         //    URL::forceScheme('https');
         //}
