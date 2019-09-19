60028f0b04025e098f7bb22d0f2b3ddb4c98dbe4
Andrés González <gandresto@outlook.com>
2019-09-19 17:31:03 -0500

    Comentario para eliminar en producción.

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,8 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+// En producción, descomentar esta línea
+/* module.exports = {
+    mode: 'production'
+} */
