934a6220a8db2dc41dffa01a96e74efde52d1538
Andrés González <gandresto@outlook.com>
2019-09-24 17:32:10 -0500

    Nuevas rutas a métodos del nuevo controlador de deptos.

--- resources/views/divisions/show.blade.php
+++ resources/views/divisions/show.blade.php
@@ -49,6 +49,20 @@
         </strong>
     </div>
 </div>
+@can('create', App\Departamento::class)
+    <div class="row">
+        <div class="col-md-12 py-2">
+            <a class="btn btn-primary" href="{{route('divisions.departamentos.create', $division->id)}}" role="button">
+                <i class="fa fa-plus" aria-hidden="true"></i>
+                <span class="ml-2">Añadir departamento</span>
+            </a>
+        </div>
+    </div>
+@endcan
+
+{{-- Barra de búsqueda próximamente aquí --}}
+
+<hr>
 @if ($division->departamentos->isEmpty())
     <div class="row">
         <div class="col-sm-12">
@@ -81,13 +95,13 @@
                                 <td>{{'placeholder-jefe'/*$departamento->jefe->grado_nombre_completo*/}}</td>
                                 {{-- @can('create', App\Academia::class) --}}
                                     <td>
-                                        <form action="{{ route('departamentos.destroy',$departamento->id) }}" method="POST">
+                                        <form action="{{ route('divisions.departamentos.destroy', [$division->id, $departamento->id]) }}" method="POST">
                                             <div class="btn-group" role="group" aria-label="Modificar departamento">
-                                                <a name="verdepartamento" href="{{route('departamentos.show',$departamento->id)}}" role="button" class="btn btn-success">
+                                                <a name="verdepartamento" href="{{route('divisions.departamentos.show', [$division->id, $departamento->id])}}" role="button" class="btn btn-success">
                                                     <i class="fas fa-eye"></i>
                                                 </a>
                                                 @can('update', $departamento)
-                                                    <a name="editardepartamento" id="editardepartamento{{$departamento->id}}" class="btn btn-primary" href="{{route('departamentos.edit', $departamento->id)}}" role="button" title="Editar">
+                                                    <a name="editardepartamento" id="editardepartamento{{$departamento->id}}" class="btn btn-primary" href="{{route('divisions.departamentos.edit', [$division->id, $departamento->id])}}" role="button" title="Editar">
                                                         <i class="fas fa-edit" aria-hidden="true"></i>
                                                     </a>
                                                 @endcan
