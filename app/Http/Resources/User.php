972dd4f5d772a0aba3cc964b4fd10935b1fd3f0c
Andrés González <gandresto@outlook.com>
2020-01-10 00:08:27 -0600

    Agrego info adicional al recurso de usuario

--- app/Http/Resources/User.php
+++ app/Http/Resources/User.php
@@ -16,14 +16,27 @@ class User extends JsonResource
     public function toArray($request)
     {
         return [
-            "id" => $this->id,
-            "grado" =>  $this->grado,
-            "nombre" =>  $this->nombre,
-            "apellido_pat" =>  $this->apellido_pat,
-            "apellido_mat" =>  $this->apellido_mat,
-            "email" =>  $this->email,
-            "api_token" => $this->when(Auth::user()->esAdmin, $this->api_token),
-
+            'id' => $this->id,
+            'grado' =>  $this->grado,
+            'nombre' =>  $this->nombre,
+            'apellido_pat' =>  $this->apellido_pat,
+            'apellido_mat' =>  $this->apellido_mat,
+            'email' =>  $this->email,
+            // 'api_token' => $this->when(Auth::user()->esAdmin, $this->api_token),
+            'info-miembro' => $this->whenPivotLoaded('academia_miembro', function () {
+                return [
+                    'actual' => $this->pivot->actual,
+                    'fecha_ingreso' => $this->pivot->fecha_ingreso,
+                    'fecha_egreso' =>  $this->pivot->fecha_egreso,
+                ];
+            }),
+            'info-presidente' => $this->whenPivotLoaded('academia_presidente', function () {
+                return [
+                    'activo' => $this->pivot->activo,
+                    'fecha_ingreso' => $this->pivot->fecha_ingreso,
+                    'fecha_egreso' =>  $this->pivot->fecha_egreso,
+                ];
+            }),
         ];
     }
 }
