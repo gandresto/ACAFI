b9ea141a4502438891713dd5135940d18eaa7ae7
Andrés González <gandresto@outlook.com>
2020-01-10 12:01:31 -0600

    Quito componente que no tengo

--- resources/js/app.js
+++ resources/js/app.js
@@ -34,7 +34,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 Vue.component('buscar-usuario', require('./components/BuscarUsuario.vue').default);
 Vue.component('buscar-division', require('./components/BuscarDivision.vue').default);
 Vue.component('editar-division', require('./components/EditarDivision.vue').default);
-Vue.component('crear-reunion', require('./components/CrearReunion.vue').default);
 Vue.component('modal', require('./components/PlantillaModal.vue').default);
 
 /**
