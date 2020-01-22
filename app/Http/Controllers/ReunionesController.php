4c5d81914833eaed5dd491892bee76923745df73
Andrés González <gandresto@outlook.com>
2020-01-22 01:37:20 -0600

    Agrego función para descargar orden del día

--- app/Http/Controllers/ReunionesController.php
+++ app/Http/Controllers/ReunionesController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Reunion;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ReunionesController extends Controller
 {
@@ -15,12 +17,26 @@ class ReunionesController extends Controller
     public function index(Request $request)
     {
         // $this->authorize('viewAny', Reunion::class);
-        return view('reuniones.index');
+        // $reuniones = Auth::user()->reuniones;
+        $reuniones = Auth::user()->reuniones->sortBy('inicio')->filter(function ($reunion) {
+            return $reunion->fin < Carbon::now();
+        });
+        return view('reuniones.index', compact('reuniones'));
     }
 
     public function create(Request $request)
     {
         $this->authorize('create', Reunion::class);
         return view('reuniones.create');
+    }
+    
+    public function descargarOrdenDelDia(int $id_reunion)
+    {
+        $reunion = Reunion::findOrFail($id_reunion);
+        if(!$reunion->orden_del_dia) abort(404);
+        else return response()->file(config('filesystems.disks.local.root')
+                                        .DIRECTORY_SEPARATOR
+                                        .$reunion->orden_del_dia
+                                    );
     }
 }
