30bbfee351ab7a15715e678784f1937f9b6d3801
Andrés González <gandresto@outlook.com>
2020-01-22 04:10:55 -0600

    Pruebas con otro formato para index

--- resources/views/reuniones/index.blade.php
+++ resources/views/reuniones/index.blade.php
@@ -27,20 +27,22 @@
 
 @if ($reunionesComoConvocado)
 <div class="row">
-    <div class="col-md-12">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
         <h4>Reuniones como miembro de academia</h4>
-        @foreach ($reunionesComoConvocado as $reunion)
+    </div>
+    @foreach ($reunionesComoConvocado as $reunion)
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
         <div class="card my-2">
             <div class="card-body">
                 @php
                     $inicio = Carbon\Carbon::parse($reunion->inicio);
                     $fin = Carbon\Carbon::parse($reunion->fin);
                 @endphp
-                <h5 class="card-title">{{$inicio->format('d/m/Y')}}. Academia de {{$reunion->academia->nombre}}</h5>
-                {{-- <p class="card-text">Fecha {{$reunion->inicio}}</p> --}}
-                <p class="card-text"></p>
-                <p class="card-text">Inicia {{$inicio->format('h:i A')}}</p>
-                <p class="card-text">Finaliza {{$fin->format('h:i A')}}</p>
+                <p class="card-text"><strong>{{$inicio->format('d/m/Y')}}</strong></p>
+                <p class="card-text">Inicia: {{$inicio->format('h:i A')}}</p>
+                <p class="card-text">Finaliza: {{$fin->format('h:i A')}}</p>
+                <p class="card-text">Lugar: {{$reunion->lugar}} </p>
+                <p class="card-text">Academia:  {{$reunion->academia->nombre}}</p>
                 @if ($reunion->orden_del_dia)
                     <a name="" id="descargar-od-{{$reunion->id}}" class="btn btn-primary" href="{{route('reuniones.ordendeldia.descargar', $reunion->id)}}" role="button" target="__blank">
                         <i class="fas fa-file-pdf"></i>
@@ -49,27 +51,29 @@
                 @endif
             </div>
         </div>
-        @endforeach
     </div>
+    @endforeach
 </div>
 @endif
 
 @if ($reunionesComoPresidente)
 <div class="row">
-    <div class="col-md-12">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
         <h4>Reuniones como presidente de academia</h4>
-        @foreach ($reunionesComoPresidente as $reunion)
+    </div>
+    @foreach ($reunionesComoPresidente as $reunion)
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
         <div class="card my-2">
             <div class="card-body">
                 @php
                     $inicio = Carbon\Carbon::parse($reunion->inicio);
                     $fin = Carbon\Carbon::parse($reunion->fin);
                 @endphp
-                <h5 class="card-title">{{$inicio->format('d/m/Y')}}. Academia de {{$reunion->academia->nombre}}</h5>
-                {{-- <p class="card-text">Fecha {{$reunion->inicio}}</p> --}}
-                <p class="card-text"></p>
-                <p class="card-text">Inicia {{$inicio->format('h:i A')}}</p>
-                <p class="card-text">Finaliza {{$fin->format('h:i A')}}</p>
+                <p class="card-text"><strong>{{$inicio->format('d/m/Y')}}</strong></p>
+                <p class="card-text">Inicia: {{$inicio->format('h:i A')}}</p>
+                <p class="card-text">Finaliza: {{$fin->format('h:i A')}}</p>
+                <p class="card-text">Lugar: {{$reunion->lugar}} </p>
+                <p class="card-text">Academia:  {{$reunion->academia->nombre}}</p>
                 @if ($reunion->orden_del_dia)
                     <a name="" id="descargar-od-{{$reunion->id}}" class="btn btn-primary" href="{{route('reuniones.ordendeldia.descargar', $reunion->id)}}" role="button" target="__blank">
                         <i class="fas fa-file-pdf"></i>
@@ -78,27 +82,29 @@
                 @endif
             </div>
         </div>
-        @endforeach
     </div>
+    @endforeach
 </div>
 @endif
 
 @if ($reunionesComoInvitadoExterno)
 <div class="row">
-    <div class="col-md-12">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
         <h4>Reuniones como invitado externo</h4>
-        @foreach ($reunionesComoInvitadoExterno as $reunion)
+    </div>
+    @foreach ($reunionesComoInvitadoExterno as $reunion)
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
         <div class="card my-2">
             <div class="card-body">
                 @php
                     $inicio = Carbon\Carbon::parse($reunion->inicio);
                     $fin = Carbon\Carbon::parse($reunion->fin);
                 @endphp
-                <h5 class="card-title">{{$inicio->format('d/m/Y')}}. Academia de {{$reunion->academia->nombre}}</h5>
-                {{-- <p class="card-text">Fecha {{$reunion->inicio}}</p> --}}
-                <p class="card-text">Format {{$reunion->fin}}</p>
-                <p class="card-text">Inicia {{$inicio->format('h:i A')}}</p>
-                <p class="card-text">Finaliza {{$fin->format('h:i A')}}</p>
+                <p class="card-text"><strong>{{$inicio->format('d/m/Y')}}</strong></p>
+                <p class="card-text">Inicia: {{$inicio->format('h:i A')}}</p>
+                <p class="card-text">Finaliza: {{$fin->format('h:i A')}}</p>
+                <p class="card-text">Lugar: {{$reunion->lugar}} </p>
+                <p class="card-text">Academia:  {{$reunion->academia->nombre}}</p>
                 @if ($reunion->orden_del_dia)
                     <a name="" id="descargar-od-{{$reunion->id}}" class="btn btn-primary" href="{{route('reuniones.ordendeldia.descargar', $reunion->id)}}" role="button" target="__blank">
                         <i class="fas fa-file-pdf"></i>
@@ -107,8 +113,8 @@
                 @endif
             </div>
         </div>
-        @endforeach
     </div>
+    @endforeach
 </div>
 @endif
 @endsection
