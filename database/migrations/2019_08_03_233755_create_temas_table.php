63410d9784bd61f10bd3e0dbcd7542868ddb7a82
Andrés González <gandresto@outlook.com>
2020-01-30 23:38:08 -0600

    Cambio el tipo del campo comentario de string a text.

--- database/migrations/2019_08_03_233755_create_temas_table.php
+++ database/migrations/2019_08_03_233755_create_temas_table.php
@@ -16,12 +16,15 @@ class CreateTemasTable extends Migration
         Schema::create('temas', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('descripcion');
-            $table->string('comentario')->nullable();
+            $table->text('comentario')->nullable();
             $table->unsignedBigInteger('reunion_id');
             $table->timestamps();
 
             $table->index('reunion_id');
-            $table->foreign('reunion_id')->references('id')->on('reunions');
+            $table->foreign('reunion_id')
+                    ->references('id')
+                    ->on('reunions')
+                    ->onDelete('cascade');
 
         });
     }
