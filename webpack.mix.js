af16e625f031d526067b90d48e7a9aa60731df4a
Andrés González <gandresto@outlook.com>
2020-02-01 23:17:11 -0600

    Empaqueto estilo para el pdf

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,9 +12,11 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-.sass('resources/sass/app.scss', 'public/css');
+    .js('resources/js/warn-exit.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/pdf.scss', 'public/css');
 
-mix.js('resources/js/warn-exit.js', 'public/js');
+// mix.js('resources/js/warn-exit.js', 'public/js');
 
 // En producción, descomentar esta línea
 /* module.exports = {
