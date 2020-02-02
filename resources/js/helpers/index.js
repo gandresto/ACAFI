169dc14a6c248a0a889a23f3f6691629dd3ccaf8
Andrés González <gandresto@outlook.com>
2020-02-02 00:55:41 -0600

    Cambio helpers y agrego uno para dar formato de fecha

--- resources/js/helpers/index.js
+++ resources/js/helpers/index.js
@@ -1,4 +1,7 @@
-exports.obtenerNombreCompleto = function (usuario) {
-    return usuario.id ? `${usuario.apellido_paterno} ${usuario.apellido_materno} ${usuario.nombre} ${usuario.grado}` : '';
-};
-exports.uuidv4 = function b(a){return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,b)};
+import {format, parseISO} from 'date-fns';
+
+export const obtenerNombreCompleto = usuario => usuario.id ? `${usuario.apellido_paterno} ${usuario.apellido_materno} ${usuario.nombre} ${usuario.grado}` : '';
+
+export const uuidv4 = function b(a){return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,b)};
+
+export const formatoFecha = ISOstring => format(parseISO(ISOstring), 'dd/MM/y');
