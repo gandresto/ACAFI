a348e5633ef74480859e074472d0bf14541474f2
Andrés González <gandresto@outlook.com>
2020-02-02 12:55:44 -0600

    Agrego función para obtener acuerdos de academias

--- app/Academia.php
+++ app/Academia.php
@@ -76,4 +76,23 @@ class Academia extends Model
         return Acuerdo::pendientes()->whereIn('tema_id', $temas_reuniones_ids)->get();
     }
 
+    /**
+     * Acuerdos que tiene esta academia. Cuando el $estado_fin es 'pend', la función 
+     * retornará sólo acuerdos pendientes. Cuando $estado_fin es 'fin', retornará 
+     * únicamente acuerdos finalizados. En cualquier otro caso, la función retornará tanto
+     * acuerdos finalizados como los pendientes.
+     * 
+     * @param string $estado_fin 
+     * @return Illuminate\Database\Eloquent\Collection
+     */
+    public function acuerdos(string $estado_fin = null)
+    {
+        $reuniones_ids = $this->reuniones->pluck('id');
+        $temas_reuniones_ids = Tema::whereIn('reunion_id', $reuniones_ids)->select('id');
+        $query = Acuerdo::whereIn('tema_id', $temas_reuniones_ids);
+        if($estado_fin == 'pend') $query = $query->pendientes();
+        else if($estado_fin == 'fin') $query = $query->finalizados();
+        return $query->get();
+    }
+
 }
