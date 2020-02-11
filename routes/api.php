30c97afc368d4be539a931921660ffd3800717ad
Andrés González <gandresto@outlook.com>
2020-02-10 21:09:24 -0600

    Cambio de resource a apiResource

--- routes/api.php
+++ routes/api.php
@@ -19,45 +19,29 @@ use Illuminate\Support\Facades\Route;
 // });
 
 // ----------- Rutas para Divisiones -------------
-Route::resource('/divisions', 'Api\DivisionController')->middleware('auth:api')->except([
-    'create', 'edit'
-]);
+Route::apiResource('/divisions', 'Api\DivisionController')->middleware('auth:api');
 Route::get('/divisions/buscar/{consulta}', 'Api\DivisionController@buscar')->middleware('auth:api');
 
 // ----------- Rutas para Usuario -------------
 Route::get('/users/yo', 'Api\UserController@yo')->middleware('auth:api');
-Route::resource('/users', 'Api\UserController')->middleware('auth:api')->except([
-    'create', 'edit'
-]);;
-Route::resource('/users/{user_id}/reuniones', 'Api\UserReunionesController')->middleware('auth:api')->except([
-    'create', 'edit'
-    ]);;
+Route::apiResource('/users', 'Api\UserController')->middleware('auth:api');
+Route::apiResource('/users/{user_id}/reuniones', 'Api\UserReunionesController')->middleware('auth:api');
 Route::get('/users/buscar/{consulta}', 'Api\UserController@buscar')->middleware('auth:api');
 Route::get('/users/{user_id}/academiasQueHaPresidido', 'Api\UserController@academiasQueHaPresidido')->middleware('auth:api');
 
 // ----------- Rutas para Academias -------------
-Route::resource('/academias', 'Api\AcademiaController')->middleware('auth:api')->except([
-    'create', 'edit'
-]);
-Route::resource('academias.acuerdos', 'Api\AcademiaAcuerdoController')->middleware('auth:api')->except([
-    'create', 'edit'
-]);
+Route::apiResource('/academias', 'Api\AcademiaController')->middleware('auth:api');
+Route::apiResource('academias.acuerdos', 'Api\AcademiaAcuerdoController')->middleware('auth:api');
 
 // ----------- Rutas para Reuniones -------------
 Route::post('/reuniones/crearPDFOrdenDelDia', 'Api\ReunionesController@crearPDFOrdenDelDia')->middleware('auth:api');
-Route::resource('/reuniones', 'Api\ReunionesController')->middleware('auth:api')->except([
-    'create', 'edit'
-]);
-Route::resource('reuniones.minuta', 'Api\ReunionesMinutaController')->middleware('auth:api')->except([
-    'create', 'edit'
-]);
+Route::apiResource('/reuniones', 'Api\ReunionesController')->middleware('auth:api');
+Route::apiResource('reuniones.minuta', 'Api\ReunionesMinutaController')->middleware('auth:api')->except('show');
 // Route::get('/reuniones/{reunion_id}/actualizarPDFOrdenDelDia', 'Api\ReunionesController@actualizarPDFOrdenDelDia')->middleware('auth:api');
 
 
 // ---------- Rutas para grados
-Route::resource('/grados', 'Api\GradosController')->middleware('auth:api')->except([
-    'create', 'edit'
-]);
+Route::apiResource('/grados', 'Api\GradosController')->middleware('auth:api');
 
 Route::fallback(function(){
     return response()->json(['message' => '¡No se encontró el recurso! Verifica la url'], 404);
