e0d916cad363c72d37315d0b98df1abf0f79a802
Andrés González <gandresto@outlook.com>
2020-02-13 11:11:24 -0600

    Modifico archivos de configuración

--- resources/js/axiosconfig.js
+++ resources/js/axiosconfig.js
@@ -1,4 +1,4 @@
-window._ = require('lodash');
+// window._ = require('lodash');
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -33,3 +33,20 @@ let api_token = document.head.querySelector('meta[name="api-token"]');
 if (api_token) {
     window.axios.defaults.headers.common['Authorization'] = 'Bearer ' + api_token.content;
 }
+
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allows your team to easily build robust real-time web applications.
+ */
+
+// import Echo from 'laravel-echo'
+
+// window.Pusher = require('pusher-js');
+
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: process.env.MIX_PUSHER_APP_KEY,
+//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+//     encrypted: true
+// });
