ac01206b4f42747dbfb895a421530810784b9c96
Andrés González <gandresto@outlook.com>
2020-02-17 14:42:22 -0600

    Cambio el botón para que haga la petición únicamente al hacer click

--- resources/js/reuniones/crear/components/SeleccionarAcademiaModal.vue
+++ resources/js/reuniones/crear/components/SeleccionarAcademiaModal.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
-    <b-button variant="primary" v-b-modal.modal-seleccionar-academia>
+    <b-button variant="primary" @click="abrirModal">
       <i class="fa fa-calendar-plus" aria-hidden="true"></i>
         <span class="ml-2">Agendar reunión</span>
     </b-button>
 
-    <b-modal id="modal-seleccionar-academia" title="Agendar reunión" v-if="academias">
+    <b-modal id="modal-seleccionar-academia" title="Agendar reunión">
       <template v-slot:default>
-        <div class="form-group">
+        <div
+            v-if="estadoAcademias == ESTADO.CARGANDO"
+            class="spinner-border spinner-border-sm mx-1"
+            role="status"
+          ><span class="sr-only">Cargando academias...</span>
+        </div>
+        <div class="form-group" v-if="estadoAcademias == ESTADO.LISTO">
           <label for="select-academia">Academia</label>
           <b-form-select
             v-model="academia_id"
@@ -43,33 +49,46 @@
 
 <script>
 import API from "../../../services/api";
+import ESTADO from "./../../../enum-estado-api";
 export default {
   mounted() {
-    let url = `${API.baseURL}/users/${window.Laravel.authUserId}/academiasQueHaPresidido?actual=1`;
-    axios.get(url)
-        .then(d => d.data)
-        .then(data => {
-          this.academias = data.data;
-        })
-        .catch(error => {
-
-        });
+    
   },
   data() {
     return {
       academias: null,
       academia_id: null,
+      ESTADO,
+      estadoAcademias: ESTADO.INICIADO,
       // academia_id: null
     }
   },
   methods: {
-
+    abrirModal(){
+      this.$bvModal.show('modal-seleccionar-academia');
+      if( !this.academias ){
+        this.estadoAcademias = this.ESTADO.CARGANDO;
+        this.obtenerAcademiasDeApi();
+      }
+    },
+    obtenerAcademiasDeApi(){
+      let url = `${API.baseURL}/users/${window.Laravel.authUserId}/academiasQueHaPresidido?actual=1`;
+      axios.get(url)
+        .then(d => d.data)
+        .then(data => {
+          this.academias = data.data;
+          this.estadoAcademias = this.ESTADO.LISTO;
+        })
+        .catch(error => {
+          this.estadoAcademias = this.ESTADO.ERROR;
+        });
+    },
   },
   computed: {
     rutaCrearReunion(){
       return this.academia_id ? 
-              `${process.env.MIX_APP_URL}/academias/${this.academia_id}/reuniones/crear` : 
-              '#';
+        `${process.env.MIX_APP_URL}/academias/${this.academia_id}/reuniones/crear` : 
+        '#';
     }
   },
 };
