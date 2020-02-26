f22f731cf47fe0bd15508082ec98e1b00785f6ae
Andrés González <gandresto@outlook.com>
2020-02-26 16:34:42 -0600

    Agrego helper para obtener cookie

--- resources/js/helpers/index.js
+++ resources/js/helpers/index.js
@@ -15,3 +15,18 @@ export const obtenerNombreCompleto = (usuario, orden='apellido', con_grado=true)
 export const uuidv4 = function b(a){return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,b)};
 
 export const formatoFecha = ISOstring => format(parseISO(ISOstring), 'dd/MM/y');
+
+export const getCookie = (cname) => {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+};
