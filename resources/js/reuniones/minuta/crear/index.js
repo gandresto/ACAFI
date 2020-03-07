ed928c7041865a18e62676a1560eeb98229f0bd8
Andrés González <gandresto@outlook.com>
2020-03-06 23:50:50 -0600

    Modifico errores con imports en minuta/crear/index.js

--- resources/js/reuniones/minuta/crear/index.js
+++ resources/js/reuniones/minuta/crear/index.js
@@ -3,8 +3,12 @@ require('../../../axiosconfig');
 import Vue from 'vue';
 import store from './store';
 import BootstrapVue from 'bootstrap-vue';
+import Autocomplete from '@trevoreyre/autocomplete-vue';
+import VueDatePicker from 'vue-bootstrap-datetimepicker';
 
 Vue.use(BootstrapVue);
+Vue.use(Autocomplete);
+Vue.use(VueDatePicker);
 
 Vue.component('crear-minuta-form', require('./components/CrearMinutaForm.vue').default);
 Vue.component('aviso-error', require('./../../../components/AvisoDeError.vue').default);
