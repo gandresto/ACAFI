bf0bfbeb7dd1c747f216ca20fc147257bd854b62
Andrés González <gandresto@outlook.com>
2020-03-09 16:50:54 -0600

    Agrego formulario al componente padre

--- resources/js/reuniones/minuta/crear/components/SeguimientoAAcuerdos.vue
+++ resources/js/reuniones/minuta/crear/components/SeguimientoAAcuerdos.vue
@@ -1,13 +1,25 @@
 <template>
   <div class="my-2">
-    <div class="row form-group">
+    <div class="row">
       <div class="col-sm-12">
-        <div class="small-title">Acuerdos a los que se les dio seguimiento</div>
+        <div class="small-title">Acuerdos de reuniones pasadas a los que se les dio seguimiento</div>
       </div>
     </div>
-    <div class="row form-group">
-      <div class="col-sm-12">
-        {{ acuerdosASeguimiento ? acuerdosASeguimiento : '' }}
+    <div class="row">
+      <div class="col-sm-10">
+        <ol>
+          <li v-for="(acuerdo, index) in acuerdosASeguimiento" :key="index">
+            <strong>{{ acuerdo.descripcion }}</strong><br/>
+            Responsable: {{ acuerdo.responsable.nombre_completo }} <br/>
+            Fecha de la reunión en que se acordó: {{ acuerdo.fecha_de_creacion | fecha }} <br/>
+            Fecha compromiso de finalización: {{ acuerdo.fecha_compromiso | fecha }} <br/>
+            Resultado/Producto esperado: {{ acuerdo.producto_esperado }} <br/>
+            <seguimiento-a-acuerdos-form
+              :acuerdo="acuerdo"
+            >
+            </seguimiento-a-acuerdos-form>
+          </li>
+        </ol>
       </div>
     </div>    
   </div>
@@ -16,10 +28,25 @@
 <script>
 
 import { mapGetters } from 'vuex';
+import SeguimientoAAcuerdosForm from './SeguimientoAAcuerdosForm.vue';
+import { formatoFecha } from "./../../../../helpers";
 
 export default {
+  components: {
+    'seguimiento-a-acuerdos-form': SeguimientoAAcuerdosForm,
+  },
+  data() {
+    return {
+
+    }
+  },
   computed: {
     ...mapGetters(['acuerdosASeguimiento']),
   },
+  filters:{
+    fecha: (datestring) => {
+      return formatoFecha(datestring);
+    }
+  }
 };
 </script>
